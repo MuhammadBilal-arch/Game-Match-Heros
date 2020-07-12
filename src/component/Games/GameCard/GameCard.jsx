0618e6fb64642d2fdf2592144e62229cc42e7f0c
Muhammad Bilal <ExpBilal@gmail.com>
2020-07-12 05:31:52 +0500

    Second Commit

--- src/component/Games/GameCard/GameCard.jsx
+++ src/component/Games/GameCard/GameCard.jsx
@@ -29,26 +29,21 @@ export class GameCard extends Component {
         if (OldCardIndex !== null) {
 
             if (data[OldCardIndex].CardID !== data[NewCardIndex].CardID) {
-                Attempts === 0 ?
-                    this.setState({ Message: "Game Over!", Error: true })
-                    :
-                    this.setState({ Attempts: Attempts - 1, Message: "Card doesnt matched!", Error: true, OldCardIndex: null })
-
-                // data[OldCardIndex].itemStatus = false;
-                // data[NewCardIndex].itemStatus = false;
+                setTimeout(function () {
+                    data[NewCardIndex].itemStatus = false;
+                    data[OldCardIndex].itemStatus = false;
+                    Attempts === 1 ? this.setState({ Message: "Game Over!", Error: true }) :
+                        this.setState({ Attempts: Attempts - 1, Message: "Attempts Left : " + (this.state.Attempts - 1), Error: true, OldCardIndex: null })
+                }.bind(this), 2000);
                 return true;
             }
 
             if (data[OldCardIndex].CardID === data[NewCardIndex].CardID) {
-                const index = data[NewCardIndex].CardID;
-                this.setState(prevState =>({ Message: "Good Job! Point +10", Error: true, OldCardIndex: null, Counter: prevState.Counter + 10 }))
-                for (var j = 0; j < 2; j++) {
-                    for (var i = 0; i < data.length; i++) {
-                        if (data[i].CardID === index) {
-                            // data.splice(i, 1)                            
-                            // data[i].itemStatus = false;
-                        }
-                    }
+                if (this.state.Counter === 75) {
+                    this.setState(prevState => ({ Message: "Congratulation You won!", Error: true, OldCardIndex: null, Counter: prevState.Counter + 25 }))
+                }
+                else {
+                    this.setState(prevState => ({ Message: "Good Job! Point + " + (this.state.Counter + 25), Error: true, OldCardIndex: null, Counter: prevState.Counter + 25 }))
                 }
                 return true;
             }
@@ -59,29 +54,37 @@ export class GameCard extends Component {
 
         const data = this.props.children
 
-        let ErrorObject = this.state.Error ? <MessageBox message={this.state.Message} score = {this.state.Counter} /> : null
+        let ErrorObject = this.state.Error ? <MessageBox message={this.state.Message} score={this.state.Counter} /> : null
 
         return (
             <section className={classes.GameSection}>
                 {ErrorObject}
-                    <div className = {classes.GameHeadRow}>                    
-                        <h3>Attempts Left : {this.state.Attempts}</h3>
-                        <h2>Match Heroes</h2>
-                        <h3>Score : {this.state.Counter}</h3>
-                    </div>
+                <div className={classes.GameHeadRow}>
+                    <h3>Attempts Left : {this.state.Attempts}</h3>
+                    <h2>Match Heroes</h2>
+                    <h3>Score : {this.state.Counter}</h3>
+                </div>
                 <div className={classes.GameCardHead}>
                     {
                         data.map((item, index) => {
                             return (
-                                <div key={index * 2} className={classes.GameCard} onClick={() => this.GameCardHandler(index)}>
-                                    {
-                                        item.itemStatus ?
-                                            <>
-                                                <h3>{item.Heading}</h3>
-                                                <img src={item.ImageURL} alt="" />
-                                            </>
-                                            : null
-                                    }
+                                <div key={index * 2} className={classes.GameCard}>
+                                    <div key={index * 2} className={classes.GameCardArea}>
+                                        <button key={index * 2}
+                                            className={item.itemStatus ? classes.GameCardFront : classes.GameCardBack}
+                                            disabled={item.itemStatus ? true : false}
+                                            onClick={() => this.GameCardHandler(index)}>
+                                            {
+                                                item.itemStatus ?
+                                                    <>
+                                                        <h3>{item.Heading}</h3>
+                                                        <img src={item.ImageURL} alt="" />
+                                                    </>
+                                                    : null
+                                            }
+                                        </button>
+                                    </div>
+
                                 </div>
                             )
                         })
